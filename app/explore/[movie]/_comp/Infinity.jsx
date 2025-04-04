1716959701ff26f4a86824f1f681a1e74aaf0268
Prabesh001 <prabeshdaahal123@gmail.com>
2025-04-04 22:48:49 +0545

    Refactor InfiniteMovies component; streamline scroll-to-top functionality and enhance user experience with improved click handling.

--- app/explore/[movie]/_comp/Infinity.jsx
+++ app/explore/[movie]/_comp/Infinity.jsx
@@ -26,25 +26,32 @@ const InfiniteMovies = ({ query }) => {
       });
     }
   }, [page]);
-  
+
   useEffect(() => {
     if (inView && !loading) {
       setPage((prevPage) => prevPage + 1);
     }
   }, [inView, loading]);
 
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <div>
       {error ? (
         <p>Error</p>
       ) : (
-        <div className="flex flex-col">
-          <Link
-            href={"#top"}
+        <div className="flex flex-col cursor-pointer">
+          <div
+            onClick={scrollToTop}
             className="fixed flex items-center justify-center pr-0 pb-2 rotate-270 right-10 bottom-10 z-30 bg-blue-400 h-12 w-12 text-5xl font-bold text-white rounded-full"
           >
             »
-          </Link>
+          </div>
           <div className="grid m-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
             {allData.length > 0 &&
               allData.map((movie, i) => (
